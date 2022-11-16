ff9bb1968bb2878f13d464a8b73045749227162d
Gerardo <jesusgrobles9@gmail.com>
2022-11-15 21:42:59 -0600

    Add galery and overlay

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
+
+//CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+
+//imagenes
+const cache = require("gulp-cache");
+const imagenmin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const avif = require("gulp-avif");
+
 
 function css(done){
     src("src/scss/**/*.scss")//Identificar el archivo de SASS
@@ -12,11 +21,61 @@ function css(done){
     done(); //Callback que avisa a gulp cuando llegamos al final
 }
 
+function versionWebp( done){
+    
+    const opciones = {
+        quality: 60
+    };
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe( webp(opciones) )
+        .pipe( dest('build/img'))
+
+    done();
+}
+
+function versionAvif( done){
+    
+    const opciones = {
+        quality: 60
+    };
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe( avif(opciones) )
+        .pipe( dest('build/img'))
+
+    done();
+}
+
+function imagenes( done){
+    
+    const opciones = {
+        optimizationLevel: 3
+    }
+    
+    src('src/img/**/*.{png,jpg}')
+        .pipe( cache( imagenmin(opciones) ) )
+        .pipe( dest('build/img'))
+    done();
+}
+
+function javaScript(done){
+    src('src/js/**/*.js')
+        .pipe(dest('build/js'))
+
+    done();
+}
+
 function dev(done){
     watch("src/scss/**/*.scss", css)
+    watch("src/js/**/*.js", javaScript)
 
     done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.js = javaScript;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel (imagenes, versionWebp, versionAvif, javaScript, dev);
