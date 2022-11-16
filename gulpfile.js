6b4fd8eb4e5d33a42e8b6726d7f66a5d70956bb7
Gerardo <jesusgrobles9@gmail.com>
2022-11-16 13:05:35 -0600

    Finish project

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,13 @@ const { src, dest, watch, parallel } = require("gulp");
 //CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const postcss = require("gulp-postcss");
+const sourcemaps = require("gulp-sourcemaps");
+
+//JavaScript
+const terser = require("gulp-terser-js");
 
 //imagenes
 const cache = require("gulp-cache");
@@ -13,10 +20,13 @@ const avif = require("gulp-avif");
 
 function css(done){
     src("src/scss/**/*.scss")//Identificar el archivo de SASS
+        .pipe( sourcemaps.init())
         .pipe( plumber() )
         .pipe( sass() )//Compilarlo
+        .pipe( postcss([ autoprefixer(), cssnano() ]))
+        .pipe( sourcemaps.write('.'))
         .pipe( dest("build/css") )//Almacenar en el disco duro
-
+    
 
     done(); //Callback que avisa a gulp cuando llegamos al final
 }
@@ -61,6 +71,9 @@ function imagenes( done){
 
 function javaScript(done){
     src('src/js/**/*.js')
+        .pipe( sourcemaps.init())
+        .pipe( terser())
+        .pipe( sourcemaps.write('.'))
         .pipe(dest('build/js'))
 
     done();
